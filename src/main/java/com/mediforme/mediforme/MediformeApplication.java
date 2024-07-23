a5d8bf7dae78fc7823f2f5486261c1980bf4ff2a
hee_!J <heeej920@naver.com>
2024-07-23 14:55:39 +0900

    feat:spring 기본 세팅 #1

---
+++ src/main/java/com/mediforme/mediforme/MediformeApplication.java
@@ -0,0 +1,13 @@
+package com.mediforme.mediforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediformeApplication.class, args);
+	}
+
+}

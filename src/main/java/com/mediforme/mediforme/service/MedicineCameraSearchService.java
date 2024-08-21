30494a2c1b3df244aa2ffeeaa8860bbff5235237
hee_!J <heeej920@naver.com>
2024-08-22 08:50:41 +0900

    feat:add max file size

--- src/main/java/com/mediforme/mediforme/service/MedicineCameraSearchService.java
+++ src/main/java/com/mediforme/mediforme/service/MedicineCameraSearchService.java
@@ -6,7 +6,6 @@ import com.google.protobuf.ByteString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;

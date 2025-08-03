b5d43f481d22a29ab960d70d2b3dccbd908cdf3a
hee_!J <heeej920@naver.com>
2025-08-03 17:07:43 +0900

    refactor: MedicineDto에 변환 로직 통합 및 Converter 클래스 제거

--- src/main/java/com/mediforme/mediforme/dto/object/MedicineDto.java
+++ src/main/java/com/mediforme/mediforme/dto/object/MedicineDto.java
@@ -7,7 +7,6 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
 public class MedicineDto {
 
     private Long id;
@@ -18,4 +17,31 @@ public class MedicineDto {
     private String component;
     private Integer amount;
 
+    public static MedicineDto fromEntity(Medicine medicine){
+        // 약 도메인 객체를 DTO로 변환
+        return MedicineDto.builder()
+                .id(medicine.getId())
+                .name(medicine.getName())
+                .description(medicine.getDescription())
+                .benefit(medicine.getBenefit())
+                .drugInteraction(medicine.getDrugInteraction())
+                //.component(medicine.getComponent())
+                //.amount(medicine.getAmount())
+                .build();
+
+    }
+
+
+    public Medicine toEntity() {
+        // DTO를 도메인 객체로 변환
+        return Medicine.builder()
+                .id(this.id)
+                .name(this.name)
+                .description(this.description)
+                .benefit(this.benefit)
+                .drugInteraction(this.drugInteraction)
+                //.component(this.component)
+                //.amount(this.amount)
+                .build();
+    }
 }

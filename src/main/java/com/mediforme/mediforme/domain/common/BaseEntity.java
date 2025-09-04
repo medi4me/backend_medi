b74eeb510cae37d89f7c0ac9b4b981af257b444c
hee_!J <heeej920@naver.com>
2025-09-04 17:38:50 +0900

    add: BaseEntity 수정자, 생성자 추가

--- src/main/java/com/mediforme/mediforme/domain/common/BaseEntity.java
+++ src/main/java/com/mediforme/mediforme/domain/common/BaseEntity.java
@@ -1,21 +1,33 @@
 package com.mediforme.mediforme.domain.common;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
+@Setter
 public class BaseEntity {
     @CreatedDate
-    private Date createdAt;
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private Date updatedAt;
+    @Column(name = "modified_at", nullable = false)
+    private Date modifiedAt;
+
+    @Column(name = "creator_id")
+    private Long creatorId;
+
+    @Column(name = "modifier_id")
+    private Long modifierId;
 }

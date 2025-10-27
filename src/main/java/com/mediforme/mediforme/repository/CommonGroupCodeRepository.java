a0bb83abe1738d28af3ce285daeb2303ffde0471
hee_!J <heeej920@naver.com>
2025-10-27 15:17:45 +0900

    add: 공통코드 체계 도입

---
+++ src/main/java/com/mediforme/mediforme/repository/CommonGroupCodeRepository.java
@@ -0,0 +1,8 @@
+package com.mediforme.mediforme.repository;
+
+import com.mediforme.mediforme.domain.code.CommonGroupCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommonGroupCodeRepository extends JpaRepository<CommonGroupCode, String> {
+
+}

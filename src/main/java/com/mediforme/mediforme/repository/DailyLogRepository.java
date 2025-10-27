83e6483d5909c7e31c0ccb6f8e259ab621592ba6
hee_!J <heeej920@naver.com>
2025-10-27 15:11:41 +0900

    refactor: Calendar 엔티티 제거 및 DailyLog, MedicineIntakeLog 구조로 변경

---
+++ src/main/java/com/mediforme/mediforme/repository/DailyLogRepository.java
@@ -0,0 +1,12 @@
+package com.mediforme.mediforme.repository;
+
+
+import com.mediforme.mediforme.domain.DailyLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface DailyLogRepository extends JpaRepository<DailyLog, Long> {
+    Optional<DailyLog> findByUserIdAndLogDate(Long userId, LocalDate logDate);  // (user_id, log_date) 유니크 조건을 기준으로 조회
+}

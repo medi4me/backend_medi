876e8c752a8c6351499541682f020e614c8dba7b
hee_!J <heeej920@naver.com>
2025-10-29 00:56:25 +0900

    chore(verification): VerificationDto 필드 및 유효성 검증 추가

--- src/main/java/com/mediforme/mediforme/dto/object/VerificationDto.java
+++ src/main/java/com/mediforme/mediforme/dto/object/VerificationDto.java
@@ -1,5 +1,7 @@
 package com.mediforme.mediforme.dto.object;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +10,8 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class VerificationDto {
+    @NotBlank(message = "전화번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^01[0-9]{8,9}$", message = "올바른 휴대폰 번호 형식이 아닙니다.")
     private String phone;
     private String verificationCode;
 

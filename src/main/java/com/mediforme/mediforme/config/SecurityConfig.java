a097a3150a886db8aa42537e7dd9e175b21cb221
hee_!J <heeej920@naver.com>
2025-11-03 13:23:41 +0900

    fix(SecurityConfig): AuthenticationManager Bean 수동 등록 추가

--- src/main/java/com/mediforme/mediforme/config/SecurityConfig.java
+++ src/main/java/com/mediforme/mediforme/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import com.mediforme.mediforme.service.TokenBlacklistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -62,6 +64,11 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class
                 );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean

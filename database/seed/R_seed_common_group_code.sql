962e234427f9181bdbfe83432de5a4830b4e8f66
hee_!J <heeej920@naver.com>
2026-01-06 17:10:34 +0900

    add: 공통코드 및 공통그룹코드 시드 데이터 추가

--- database/seed/R_seed_common_group_code.sql
+++ database/seed/R_seed_common_group_code.sql
@@ -6,4 +6,8 @@ INSERT INTO t_common_group_code (
 ) VALUES
       ('ROLE',   '사용자 역할', NOW(), NOW()),
       ('STATUS', '사용자 상태', NOW(), NOW()),
-      ('CONSENT','약관 동의 상태', NOW(), NOW());
+      ('CONSENT','약관 동의 상태', NOW(), NOW()),
+      ('MEAL_CD',        '식사 구분',   NOW(6), NOW(6)),
+      ('TIME_CD',        '복용 시간',   NOW(6), NOW(6)),
+      ('DAYS_OF_WEEK_CD','요일',       NOW(6), NOW(6)),
+      ('ALARM_YN',       '알람 여부',   NOW(6), NOW(6));

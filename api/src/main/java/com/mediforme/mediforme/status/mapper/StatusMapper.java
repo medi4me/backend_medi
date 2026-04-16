cdeaaaa8c0c94bb04ea4a9b988c0791a5f23f4a1
hee_!J <heeej920@naver.com>
2026-04-16 10:28:45 +0900

    fix: StatusMapper Lombok-MapStruct 바인딩 추가 및 매핑 명시

--- api/src/main/java/com/mediforme/mediforme/status/mapper/StatusMapper.java
+++ api/src/main/java/com/mediforme/mediforme/status/mapper/StatusMapper.java
@@ -1,13 +1,21 @@
 package com.mediforme.mediforme.status.mapper;
 
 import com.mediforme.mediforme.status.domain.Status;
-import com.mediforme.mediforme.status.dto.StatusAdminRequestDto;
 import com.mediforme.mediforme.status.dto.StatusAdminResponseDto;
 import com.mediforme.mediforme.status.dto.StatusMeResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StatusMapper {
+
+    @Mapping(target = "defaultStatusName", ignore = true)
+    @Mapping(target = "drinkName", ignore = true)
+    @Mapping(target = "conditionName", ignore = true)
     StatusMeResponseDto toMeResponse(Status entity);
+
+    @Mapping(target = "defaultStatusName", ignore = true)
+    @Mapping(target = "drinkName", ignore = true)
+    @Mapping(target = "conditionName", ignore = true)
     StatusAdminResponseDto toAdminResponse(Status entity);
 }

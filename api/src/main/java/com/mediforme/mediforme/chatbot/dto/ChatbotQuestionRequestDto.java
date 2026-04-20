78f5652a44a2eee88cddb02511a49f2df676ec4e
hee_!J <heeej920@naver.com>
2026-04-20 12:35:51 +0900

    feat: 챗봇 요청 DTO에 약 컨텍스트 필드 추가

--- api/src/main/java/com/mediforme/mediforme/chatbot/dto/ChatbotQuestionRequestDto.java
+++ api/src/main/java/com/mediforme/mediforme/chatbot/dto/ChatbotQuestionRequestDto.java
@@ -1,11 +1,19 @@
 package com.mediforme.mediforme.chatbot.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Getter
+@NoArgsConstructor
 public class ChatbotQuestionRequestDto implements Serializable {
-    @jakarta.validation.constraints.NotBlank(message = "질문 내용은 필수 입력 항목입니다.")
+
+    @NotBlank(message = "질문 내용은 필수 입력 항목입니다.")
     private String question;
+
+    @Valid
+    private MedicineContextDto medicineContext;
 }
